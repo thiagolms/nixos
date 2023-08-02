00c1ed44a279a97c89064ea149f1718ad0c0955a
thiagolms <thiagopinheiiro@live.com>
2023-08-02 01:44:24 -0300

    config atualizada

--- configuration.nix
+++ configuration.nix
@@ -99,11 +99,11 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-	git,
-	vscode,
-	notepadqq,
-	dbeaver,
-	arduino,
+	git
+	vscode
+	notepadqq
+	dbeaver
+	arduino
 	btop
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
